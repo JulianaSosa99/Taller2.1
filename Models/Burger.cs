2e53468d891065249101770248722aa13c5a31b3
Juliana_Sosa <juliana.sosa@udla.edu.ec>
2024-04-10 11:21:36 -0500

    Comentario en clase Burger.cs

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,6 +1,17 @@
-﻿namespace Juliana_Sosa_Taller1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Juliana_Sosa_Taller1.Models
 {
     public class Burger
+
     {
+        public int BurgerID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese {  get; set; }
+        [Range(0.01, 9999.9)] //el rango del siguiente atributo
+        public decimal Precio { get; set; }
+        //hola
+
     }
 }
