15e92ea4251593d60ecb6e5c1110f402dc2805d6
Kamila <juliana.sosa@udla.edu.ec>
2024-04-17 12:07:14 -0500

    Creacion de la clase promo

---
+++ Models/Promo.cs
@@ -0,0 +1,13 @@
+﻿namespace Juliana_Sosa_Taller1.Models
+{
+    public class Promo
+    {
+        public int PromoID { get; set; }
+        public string? Descripcion { get; set;}
+        public DateTime? FechaPromo { get; set; }
+        public int BurgerID {  get; set; }
+        public Burger? Burger { get; set; }
+
+
+    }
+}
